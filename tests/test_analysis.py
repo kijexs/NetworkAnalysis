cf4ebb70d84201c21814029b188b2f2d7531c127
Ekaterina Arzamaszeva <arz_kati@mail.ru>
2026-05-02 01:19:34 +0300

    test: add tests for distance estimation methods

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,5 +1,11 @@
 # тесты метрик
-from src.analysis import connected_components, largest_cc_size
+from src.analysis import (
+    connected_components,
+    largest_cc_size,
+    double_sweep_diameter,
+    sampled_diameter_and_percentile,
+    snowball_diameter_percentile,
+)
 from src.graph import Graph
 
 
@@ -30,3 +36,37 @@ def test_largest_cc():
     size, frac = largest_cc_size(g)
     assert size == 3
     assert frac == 3 / 5
+
+
+def test_double_sweep_simple():
+    g = Graph()
+    g.add_edge(1, 2)
+    g.add_edge(2, 3)
+    g.add_edge(3, 1)  # треугольник
+    cc = {1, 2, 3}
+    diam, perc = double_sweep_diameter(g, cc, percentile=90)
+    assert diam == 1
+    assert perc == 1
+
+
+def test_sample_distances():
+    g = Graph()
+    # 1-2-3-4
+    for i in range(1, 4):
+        g.add_edge(i, i + 1)
+    cc = {1, 2, 3, 4}
+    diam, perc = sampled_diameter_and_percentile(g, cc, sample_size=100, percentile=90)
+    assert diam == 3
+    assert perc <= 3
+
+
+def test_snowball():
+    g = Graph()
+    for i in range(10):
+        for j in range(i + 1, 10):
+            g.add_edge(i, j)
+    cc = set(range(10))
+    diam, perc = snowball_diameter_percentile(g, cc, target_size=500, percentile=90)
+    # в полном графе диаметр == 1
+    assert diam == 1
+    assert perc == 1
